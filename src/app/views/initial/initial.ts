7108cac8bee199643b93368976e9ae2e86b26d59
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2025-10-27 13:50:45 -0300

    Add StartPage component with HTML, CSS, and test files; update initial view to include StartPage

--- src/app/views/initial/initial.ts
+++ src/app/views/initial/initial.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from '../components/header/header';
 import { Footer } from '../components/footer/footer';
 import { Login } from '../login/login';
+import { StartPage } from './start-page/start-page';
 
 @Component({
   selector: 'app-initial',
-  imports: [Header, Footer, Login],
+  imports: [Header, Footer, Login, StartPage],
   templateUrl: './initial.html',
   styleUrl: './initial.css',
 })
