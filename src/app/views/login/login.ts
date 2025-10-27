6ab043abf1ea029f10bf7400ce3ad749140abbb0
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2025-10-27 10:58:43 -0300

    Refactor login component: implement reactive forms for login and registration, update styles for disabled buttons, and enhance form validation

--- src/app/views/login/login.ts
+++ src/app/views/login/login.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -15,5 +16,33 @@ export class Login {
 
   activeLogin() {
     this.isActive = false;
+  }
+
+  private formBuilder: FormBuilder = inject(FormBuilder);
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  onSubmitLogin() {
+    if (this.loginForm.valid) {
+      alert('Login Successful');
+    } else {
+      alert('Login Form is invalid');
+    }
+  }
+
+  onSubmitRegister() {
+    if (this.registerForm.valid) {
+      alert('Registration Successful');
+    } else {
+      alert('Registration Form is invalid');
+    }
   }
 }
